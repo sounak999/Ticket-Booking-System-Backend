e76d2b873bc3c72d41073b6e78e9d3ff7db8b3de
Sounak <sounaksaha333@gmail.com>
2025-03-17 01:10:33 +0530

    0.2 => READ user list from local db

--- ticket-booking/app/src/main/java/ticket/booking/services/UserBookingService.java
+++ ticket-booking/app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -1,12 +1,23 @@
 package ticket.booking.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.entities.User;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class UserBookingService {
     private User user;
+    private List<User> userList;
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final String USERS_PATH = "ticket-booking/app/src/main/java/ticket/booking/localDb/users.json";
 
-    UserBookingService(User user) {
+    UserBookingService(User user) throws IOException {
         this.user = user;
+        File file = new File(USERS_PATH);
+        userList = objectMapper.readValue(file, new TypeReference<List<User>>() {});
     }
 
 }
