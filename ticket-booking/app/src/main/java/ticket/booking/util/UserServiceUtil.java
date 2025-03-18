d9ed2007d45bf85c901487ec411a13056e2c37a6
Sounak <sounaksaha333@gmail.com>
2025-03-18 21:41:55 +0530

    0.3 => encrypt password using java Bcrypt

---
+++ ticket-booking/app/src/main/java/ticket/booking/util/UserServiceUtil.java
@@ -0,0 +1,13 @@
+package ticket.booking.util;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class UserServiceUtil {
+    public static String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public static boolean checkPassword(String normalPassword, String hashedPassword) {
+        return BCrypt.checkpw(normalPassword, hashedPassword);
+    }
+}
